c1e99efbbaad21d946c17e243e39e6bd59227946
alex <alexsey_89@bk.ru>
2021-01-24 00:07:55 +0300

    added library for php project

--- public/index.php
+++ public/index.php
@@ -2,4 +2,21 @@
 
 declare(strict_types=1);
 
-phpinfo();
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Проектируем систему без фреймворков
+
+// Подключаем обработчик ошибок
+// docker-compose run --rm --user="root" php-cli-alpine composer require filp/whoops
+
+$whoops = new Run();
+$whoops->pushHandler(new PrettyPageHandler);
+$whoops->register();
+
+// Подключаем библиотеку для работы с http
+// docker-compose run --rm --user="root" php-cli-alpine composer require slim/psr7
+
+//$request = new \Slim\Psr7\Request();
